dbe7a23e83c231814db0b33af2f0ba7cdb004362
Kyle McGuffin <kyle.mcguffin@vumc.org>
2022-09-18 14:31:28 -0400

    Start launches per year metric with var_dump

--- report.php
+++ report.php
@@ -1,2 +1,28 @@
 <?php
 /** @var $module \REDCapCon\REDCapConTestModule\REDCapConTestModule */
+include_once(\ExternalModules\ExternalModules::getProjectHeaderPath());
+
+$recordData = \REDCap::getData([
+	"return_format" => "json",
+	"project_id" => $project_id
+]);
+$recordData = json_decode($recordData,true);
+
+$launchesPerYear = [];
+
+foreach($recordData as $thisRecord) {
+	$launchYear = $thisRecord["launch_date"] ?: "";
+	$launchYear = (int)substr($launchYear,0,4);
+	
+	if($launchYear != 0) {
+		if(!array_key_exists($launchYear,$launchesPerYear)) {
+			$launchesPerYear[$launchYear] = 0;
+		}
+		
+		$launchesPerYear[$launchYear]++;
+	}
+}
+
+echo "<br /><pre>";
+var_dump($launchesPerYear);
+echo "</pre><br />";
