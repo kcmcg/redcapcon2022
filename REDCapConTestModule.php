cd4e199b2cc0ed2142945c545909f610f36f4a95
Kyle McGuffin <kyle.mcguffin@vumc.org>
2023-10-04 16:01:59 -0400

    Update to actually be able to generate test data

--- REDCapConTestModule.php
+++ REDCapConTestModule.php
@@ -23,34 +23,34 @@ class REDCapConTestModule extends \ExternalModules\AbstractExternalModule {
 
 	
 	public function redcap_data_entry_form( $project_id, $record, $instrument, $event_id, $group_id, $repeat_instance ) {
-		$data = \REDCap::getData([
-			"project_id" => $project_id,
-			"records" => $record,
-			"fields" => "launch_date",
-			"return_format" => "json"
-		]);
-		
-        $time = time();
-		$data = json_decode($data,true);
-		
-		$upcomingLaunch = false;
-		foreach($data as $thisRow) {
-			$launch = $thisRow["launch_date"] ? strtotime($thisRow["launch_date"]) : 0;
-			if($launch > $time && $launch < ($time + 60*60*24*90)) {
-				$upcomingLaunch = true;
-				break;
-			}
-		}
-		
-		if($upcomingLaunch) {
-			?>
-            <script type="text/javascript">
-                $(document).ready(function() {
-                    $("#dataEntryTopOptions").after("<div style='background-color:red;color:white;font-weight:bold;max-width:300px;text-align:center;padding:10px'>Upcoming!</div>");
-                });
-            </script>
-			<?php
-		}
+//		$data = \REDCap::getData([
+//			"project_id" => $project_id,
+//			"records" => $record,
+//			"fields" => "launch_date",
+//			"return_format" => "json"
+//		]);
+//
+//        $time = time();
+//		$data = json_decode($data,true);
+//
+//		$upcomingLaunch = false;
+//		foreach($data as $thisRow) {
+//			$launch = $thisRow["launch_date"] ? strtotime($thisRow["launch_date"]) : 0;
+//			if($launch > $time && $launch < ($time + 60*60*24*90)) {
+//				$upcomingLaunch = true;
+//				break;
+//			}
+//		}
+//
+//		if($upcomingLaunch) {
+//			?>
+<!--            <script type="text/javascript">-->
+<!--                $(document).ready(function() {-->
+<!--                    $("#dataEntryTopOptions").after("<div style='background-color:red;color:white;font-weight:bold;max-width:300px;text-align:center;padding:10px'>Upcoming!</div>");-->
+<!--                });-->
+<!--            </script>-->
+<!--			--><?php
+//		}
 	}
 
 	
@@ -85,35 +85,35 @@ class REDCapConTestModule extends \ExternalModules\AbstractExternalModule {
 
 	
 	public function redcap_save_record( $project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id, $repeat_instance ) {
-		$data = \REDCap::getData([
-			"project_id" => $project_id,
-			"records" => $record,
-			"fields" => "launch_date",
-			"return_format" => "json"
-		]);
-		$recordData = json_decode($data,true);
-        
-        $launchDate = reset($recordData)["launch_date"];
-        
-        $q = $this->queryLogs("SELECT launch_date WHERE record = ? AND project_id = ?", [$record, $project_id]);
-        $latestLaunch = false;
-        
-        while($row = db_fetch_assoc($q)) {
-	        $latestLaunch = $row["launch_date"];
-		}
-        
-        if($launchDate != $latestLaunch) {
-            if($launchDate > $latestLaunch && $latestLaunch !== false) {
-    	        $this->log("Launch Delayed",["record" => $record, "project_id" => $project_id, "launch_date" => $launchDate]);
-                \REDCap::logEvent("Launch Delayed","","",$record,null,$project_id);
-			}
-            else if($latestLaunch === false) {
-    			$this->log("New Launch",["record" => $record, "project_id" => $project_id, "launch_date" => $launchDate]);
-			}
-            else {
-				$this->log("Launch Pulled Ahead",["record" => $record, "project_id" => $project_id, "launch_date" => $launchDate]);
-			}
-		}
+//		$data = \REDCap::getData([
+//			"project_id" => $project_id,
+//			"records" => $record,
+//			"fields" => "launch_date",
+//			"return_format" => "json"
+//		]);
+//		$recordData = json_decode($data,true);
+//
+//        $launchDate = reset($recordData)["launch_date"];
+//
+//        $q = $this->queryLogs("SELECT launch_date WHERE record = ? AND project_id = ?", [$record, $project_id]);
+//        $latestLaunch = false;
+//
+//        while($row = db_fetch_assoc($q)) {
+//	        $latestLaunch = $row["launch_date"];
+//		}
+//
+//        if($launchDate != $latestLaunch) {
+//            if($launchDate > $latestLaunch && $latestLaunch !== false) {
+//    	        $this->log("Launch Delayed",["record" => $record, "project_id" => $project_id, "launch_date" => $launchDate]);
+//                \REDCap::logEvent("Launch Delayed","","",$record,null,$project_id);
+//			}
+//            else if($latestLaunch === false) {
+//    			$this->log("New Launch",["record" => $record, "project_id" => $project_id, "launch_date" => $launchDate]);
+//			}
+//            else {
+//				$this->log("Launch Pulled Ahead",["record" => $record, "project_id" => $project_id, "launch_date" => $launchDate]);
+//			}
+//		}
 	}
 
 	
@@ -180,13 +180,13 @@ class REDCapConTestModule extends \ExternalModules\AbstractExternalModule {
 	
 
 	public function run_cron( $cronParameters ) {
-		$launchData = $this->connectToSpaceXAPI("launches");
-		
-		$projects = $this->framework->getProjectsWithModuleEnabled();
-		
-		foreach($projects as $projectId) {
-			$this->saveLaunchDataToProject($projectId, $launchData);
-		}
+//		$launchData = $this->connectToSpaceXAPI("launches");
+//
+//		$projects = $this->framework->getProjectsWithModuleEnabled();
+//
+//		foreach($projects as $projectId) {
+//			$this->saveLaunchDataToProject($projectId, $launchData);
+//		}
 	}
 	
 	public function saveLaunchDataToProject( $project_id, $launchData) {
--- testData.php
+++ testData.php
@@ -1,12 +1,145 @@
 <?php
-if($_POST['newRecordCount']) {
-    $recordIdField = $this->getRecordIdField($project_id);
-    $metadata = $module->getMetadata();
-    $sampleRecord = [];
-    foreach($metadata as $field) {
-        if($module->isRepeatingForm($field['field_name'])) {
-            for($i = 1; $i <= $_POST['repeatInstanceCount'];$i++) {
+/** @var $module \REDCapCon\REDCapConTestModule\REDCapConTestModule */
+if(!$module->getUser()->hasDesignRights()) {
+	die();
+}
+
+$recordCount = (int)$_POST['newRecordCount'];
+$instanceCount = (int)$_POST['repeatInstanceCount'];
+$formToInstanceMap = [];
+
+if($recordCount) {
+	$repeatingForms = $module->getRepeatingForms();
+    $recordIdField = $module->getRecordIdField();
+    $metadata = $module->getMetadata($project_id);
+    $sampleRecord = [[]];
+    foreach($metadata as $field => $fieldDetails) {
+		## Skip record ID field for sample record
+		if($field == $recordIdField ||
+                in_array($fieldDetails["field_type"], ["calc","descriptive"]) ||
+                strpos($fieldDetails["field_annotation"],"@CALCTEXT") !== false ||
+			    strpos($fieldDetails["field_annotation"],"@CALCDATE") !== false) {
+			continue;
+		}
+		$formName = $module->getFormForField($field);
+
+        if(in_array($formName,$repeatingForms)) {
+            for($i = 1; $i <= $instanceCount;$i++) {
+                if(!array_key_exists($formName,$formToInstanceMap)) {
+					$currentRepeatForm = count($formToInstanceMap);
+                    for($newInstance = 1; $newInstance <= $instanceCount; $newInstance++) {
+                        $instance = $newInstance + $currentRepeatForm * $instanceCount;
+                        $sampleRecord[$instance]["redcap_repeat_instance"] = $newInstance;
+                        $sampleRecord[$instance]["redcap_repeat_instrument"] = $formName;
+                        $formToInstanceMap[$formName][$newInstance] = $instance;
+					}
+				}
+                
+                $instance = $formToInstanceMap[$formName][$i];
+                
+                addItemToSampleData($sampleRecord,$field,$instance,$fieldDetails);
             }
         }
+		else {
+			addItemToSampleData($sampleRecord,$field,0,$fieldDetails);
+		}
     }
+    $time1 = 0.0;
+    $time2 = 0.0;
+	echo "<br /><pre>";
+	var_dump($sampleRecord);
+	echo "</pre><br />";
+	
+    ## Generate new record ID for first record
+	$newRecord = \REDCap::reserveNewRecordId($project_id);
+	$saveData = [];
+	for($i = 0; $i < $recordCount; $i++) {
+		$startTime = microtime(true);
+		$endTime = microtime(true);
+        $time1 += $endTime - $startTime;
+        $startTime = $endTime;
+		foreach($sampleRecord as $instance => $instanceDetails) {
+			$saveData[] = $instanceDetails;
+			$saveData[count($saveData) - 1][$recordIdField] = $newRecord;
+		}
+        
+        ## If continuing to generate new records, generate next
+        ## Doing this here allows us to start with suspected next record ID,
+        ## which speeds up reserveNewRecordId greatly
+        if($i < ($recordCount - 1)) {
+			$newRecord = \REDCap::reserveNewRecordId($project_id, $newRecord + 1);
+        }
+		$endTime = microtime(true);
+		$time2 += $endTime - $startTime;
+		$startTime = $endTime;
+	}
+    echo "Time: $time1 ~ $time2 <Br />";
+    
+//	echo "<br /><pre>";
+//	var_dump($metadata);
+//	echo "</pre><br />";
+//	echo "<br /><pre>";
+//	var_dump($saveData);
+//	echo "</pre><br />";
+	$results = \REDCap::saveData([
+		"project_id" => $project_id,
+		"data" => json_encode($saveData),
+		"dataFormat" => "json"
+	]);
+	echo "<br /><pre>";
+	var_dump($results);
+	echo "</pre><br />";
 }
+
+function addItemToSampleData(&$sampleData, $field, $instance, $fieldDetails) {
+    global $module, $project_id;
+    
+	## TODO Need to check field type and validation type
+	if($fieldDetails["field_type"] == "text") {
+		if(substr($fieldDetails["text_validation_type_or_show_slider_number"],0,5) == "date_") {
+			$sampleData[$instance][$field] = "2022-01-01";
+		}
+		else if(substr($fieldDetails["text_validation_type_or_show_slider_number"], 0,9) == "datetime_") {
+			$sampleData[$instance][$field] = "2022-01-01 01:01:01";
+		}
+        else if($fieldDetails["text_validation_type_or_show_slider_number"] == "time") {
+			$sampleData[$instance][$field] = "01:01";
+		}
+		else if($fieldDetails["text_validation_type_or_show_slider_number"] == "number" || $fieldDetails["text_validation_type_or_show_slider_number"] == "integer") {
+			$sampleData[$instance][$field] = floor(rand(1,20));
+		}
+		else {
+			$sampleData[$instance][$field] = "rock";
+		}
+	}
+	else if($fieldDetails["select_choices_or_calculations"] != "") {
+		$types = $module->getChoiceLabels($field,$project_id);
+		$rand = floor(rand(0,count($types)));
+		foreach($types as $rawValue => $label) {
+			if($rand == 0) {
+				if($fieldDetails["field_type"] == "checkbox") {
+					$sampleData[$instance][$field."___".$rawValue] = 1;
+				}
+				else {
+					$sampleData[$instance][$field] = $rawValue;
+				}
+				break;
+			}
+			$rand--;
+		}
+	}
+	
+}
+
+include_once(\ExternalModules\ExternalModules::getProjectHeaderPath());
+?>
+
+<form method="post">
+    <div>Number of Records to Generate</div>
+	<input type="text" name="newRecordCount" value="<?=$recordCount?>" /><br /><br />
+
+    <div>Instances to Generate for each Record</div>
+	<input type="text" name="repeatInstanceCount" value="<?=$instanceCount?>" /><br /><br />
+    
+    <input type="submit" value="Generate Records" />
+</form>
